27f70668984de2a0c0f760ac9e3cac1acda3764b
Tobdu399 <topias.niiranen12@gmail.com>
2020-12-08 18:45:42 +0200

    Clickable buttons added

--- buttons.js
+++ buttons.js
@@ -3,11 +3,32 @@ let row2_buttons = [];
 
 class Button {
   constructor(x, y, width, height, borderradius) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.borderradius = borderradius;
+    this.isClicked = false;
 
     this.show = function() {
       fill(255);
       noStroke();
-      rect(x*scale, y*scale, width*size*scale, height*size*scale, borderradius);
+      rect(this.x*scale, this.y*scale, this.width*size*scale, this.height*size*scale, this.borderradius);
+
+      if (this.isClicked) {
+        fill(200);
+        noStroke();
+        rect(this.x*scale, this.y*scale, this.width*size*scale, this.height*size*scale, this.borderradius);
+      }
+    }
+
+    this.click = function() {      
+      if (this.isClicked == false && mouseX > this.x && mouseX < this.x+this.width && mouseY > this.y && mouseY < this.y+this.height) {
+        this.isClicked = true;
+        setTimeout(() => {
+          this.isClicked = false;
+        }, 150);
+      }
     }
 
   }
@@ -19,8 +40,8 @@ function drawButtons() {
 
   if (row1_buttons.length <= buttonAmount || row2_buttons.length <= buttonAmount) {
     for (let i=1; i<buttonAmount; i++) {
-      const button1 = new Button(102*i, 100, 90, 120, 5);
-      const button2 = new Button(102*i, 235, 90, 120, 5)
+      const button1 = new Button(102*i, 150, 90, 120, 5);
+      const button2 = new Button(102*i, 285, 90, 120, 5)
 
       if (row1_buttons.length <= buttonAmount)
         row1_buttons.push(button1);
@@ -33,4 +54,11 @@ function drawButtons() {
     for (let k=0; k<row2_buttons.length; k++)
       row2_buttons[k].show();
   }
+}
+
+function mouseClicked() {
+  for (let j=0; j<row1_buttons.length; j++)
+      row1_buttons[j].click();
+    for (let k=0; k<row2_buttons.length; k++)
+      row2_buttons[k].click();
 }
--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 let width = 700,
     scale = 1,
-    size = 1,
-    buttonAmount = 6,
-    scale_slider = document.getElementById("scale-slider"),
-    size_slider = document.getElementById("size-slider"),
-    boldFont,
+    size = 1;
+
+let buttonAmount = 6;
+
+let scale_slider = document.getElementById("scale-slider"),
+    size_slider = document.getElementById("size-slider");
+
+let boldFont,
     lightFont;
 
 function setup() {
@@ -16,16 +19,14 @@ function setup() {
 
 function draw() {
   // Using type coercion to be able to format the value to 2 decimals
-  scale = (scale_slider.value*1).toFixed(2);
-  document.getElementById("scale-title").innerHTML = "Scale: " + scale + "x";
+  scale = scale_slider.value*1;
+  document.getElementById("scale-title").innerHTML = "Scale: " + (scale*100).toFixed(0) + "%";
 
-  size = (size_slider.value*1).toFixed(2);
-  document.getElementById("size-title").innerHTML = "Size: " + size + "x";
+  size = size_slider.value*1;
+  document.getElementById("size-title").innerHTML = "Size:  " + (size*100).toFixed(0) + "%";
 
   resizeCanvas(width*scale, (width/1.5)*scale)
   background(220);
-
-  textSize(20*size*scale);
 
   // --> Game Loop
   // Title
@@ -33,7 +34,15 @@ function draw() {
   textFont(boldFont);
   textAlign(CENTER);
   textStyle(BOLD);
+  textSize(20*size*scale);
   text("Game", width/2*scale, 50*scale);
+
+  // Balance
+  fill(0);
+  textFont(lightFont);
+  textAlign(LEFT);
+  textSize(16*size*scale);
+  text("Money: ", 100*scale, 100*scale);
 
 
   drawButtons();
